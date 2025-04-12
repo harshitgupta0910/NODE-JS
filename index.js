6b9868dd2d22951d90121684fe595f83206d327f
Harshit Gupta <harshitgupta0910@gmail.com>
2025-04-12 16:47:43 +0530

    use of rest api

--- index.js
+++ index.js
@@ -1,44 +1,88 @@
 const express = require('express');
 const users = require("./MOCK_DATA.json");
+const fs = require('fs');
 const app = express();
 const port = 1000;
+
+// Middleware to parse request bodies
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// GET: Return all users as JSON
 app.get('/api/users', (req, res) => {
     return res.json({ users });
 });
 
+// GET: Return users as an HTML list
 app.get('/users', (req, res) => {
     const html = `
     <ul>
-    ${users.map(users => `<li>${users.first_name}</li>`).join('')} // this will return the first name of all users in the list
+        ${users.map(user => `<li>${user.first_name}</li>`).join('')}
     </ul>
     `;
     res.send(html);
 });
 
-// app.get('/api/users/:id', (req, res) => {
-//     const id = Number(req.params.id);
-//     const user = users.find((user) => user.id === id);
-//     return res.json({ user }); // this will return the user object with the given id
-// });
-
-app
-  .route('/api/users/:id') //make routes of same type together having different methods
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json({ user });
-  })
-    .put((req, res) => {
-        return res.json({ status : "Pending" }); 
+// ROUTE: Handle /api/users/:id for GET, PATCH, DELETE
+app.route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find(user => user.id === id);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        return res.json({ user });
     })
     .patch((req, res) => {
-        return res.json({ status : "Editing" }); 
+        const id = Number(req.params.id);
+        const userIndex = users.findIndex(user => user.id === id);
+        if (userIndex === -1) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const updatedUser = {
+            ...users[userIndex],
+            ...req.body
+        };
+
+        users[userIndex] = updatedUser;
+
+        fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users, null, 2));
+        return res.json({ status: "User updated", user: updatedUser });
     })
     .delete((req, res) => {
-        return res.json({ status : "Deleting" }); 
+        const id = Number(req.params.id);
+        
+        const userIndex = users.findIndex(user => user.id === id);
+        if (userIndex === -1) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        users.splice(userIndex, 1);
+        fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users, null, 2));
+        return res.json({ status: "User deleted" });
     });
 
-app.listen
-(port, () => {
+// POST: Add a new user
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+
+    if (!body || typeof body !== 'object') {
+        return res.status(400).json({ error: "Invalid request body" });
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        ...body
+    };
+
+    users.push(newUser);
+    fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users, null, 2));
+
+    return res.status(201).json({ status: "User added", user: newUser });
+});
+
+// Start the server
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
